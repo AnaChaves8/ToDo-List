e95d2e03c0f41f566eb335a3631ca57a87a9fd75
Ana Chaves <anajuchaves03@gmail.com>
2023-01-16 20:56:58 -0300

    finished header

---
+++ ToDo-List/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+import Logo from '../../assets/Layer2.svg'
+import todo from '../../assets/todo.svg'
+
+export function Header(){
+  return(
+    <header className={styles.header}>
+      <img className={styles.logo} src={Logo} alt="Logo ToDo" />
+      <img className={styles.todo} src={todo} alt="Logo Todo" />
+    </header>
+  )
+}
